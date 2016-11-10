eed7a29fec63af0e28dface6da1ca374e645171f
operario <operario@www.velocimetro.org.velocimetro.org>
2016-11-10 10:44:21 +0100

    Store model-based traces under the same folder

--- testKiller.sh
+++ testKiller.sh
@@ -3,6 +3,9 @@
 cd /home/operario/maril-in-monroe-TCPserver
 first=`ps -ef | grep '[ssTra]ce' | head -1 |awk '{print $2}'`
 nameFolder=`ps -ef | grep 'ssTrace' | awk '{print $11}' | head -1`
+year=`echo "$nameFolder" | cut -d - -f 1`
+month=`echo "$nameFolder" | cut -d - -f 2`
+day=`echo "$nameFolder" | cut -d - -f 3 | cut -d _ -f 1`
 ip=`ps -ef | grep 'ssTrace' | awk '{print $10}' | head -1`
 if [ "$first" != "" ]
 then
@@ -19,6 +22,8 @@ then
 	mv "$nameFolder"_strip.pcap "$nameFolder"_"$ip"_"$1"_"$2"_folder/"$nameFolder"_"$ip"_"$1"_"$2"_strip.pcap
 	mv "$nameFolder"_ss.txt "$nameFolder"_"$ip"_"$1"_"$2"_folder/"$nameFolder"_"$ip"_"$1"_"$2"_ss.txt
 	mv output* "$nameFolder"_"$ip"_"$1"_"$2"_folder/
+	nameModel=`ls ../maril-in-monroe-Model-based/"$year"/"$month"/"$day"/*serverdata | tail -1 | cut -d . -f 4 | cut -d _ -f 1`
+	cp ../maril-in-monroe-Model-based/"$year"/"$month"/"$day"/*"$nameModel"* "$nameFolder"_"$ip"_"$1"_"$2"_folder/
 else
 	nameFail=`ls | grep '\.pcap' |sed 's/\.pcap//'`
 	editcap -C 122:1420 "$nameFail".pcap "$nameFail"_strip.pcap
@@ -27,5 +32,6 @@ else
         mv "$nameFail".pcap "$nameFail"_"$1"_"$2"_FailedFolder/"$nameFail"_"$1"_"$2".pcap
 	mv "$nameFail"_strip.pcap "$nameFail"_"$1"_"$2"_FailedFolder/"$nameFail"_"$1"_"$2"_strip.pcap
         mv "$nameFail"_ss.txt "$nameFail"_"$1"_"$2"_FailedFolder/"$nameFail"_"$1"_"$2"_ss.txt
-
+	nameModel=`ls ../maril-in-monroe-Model-based/"$year"/"$month"/"$day"/*serverdata | tail -1 | cut -d . -f 4 | cut -d _ -f 1`
+	cp ../maril-in-monroe-Model-based/"$year"/"$month"/"$day"/*"$nameModel"* "$nameFail"_"$1"_"$2"_FailedFolder/
 fi
